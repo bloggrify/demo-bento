b6ac9e0a145ae27be462bed9a7d16bc068e0a689
hlassiege <hlassiege@gmail.com>
2024-05-27 18:49:55 +0200

    chore: initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,25 @@
+
+export default defineNuxtConfig({
+    app: {
+        head: {
+            script: [
+                ...(process.env.PIRSCH_CODE
+                    ? [
+                        {
+                            src: 'https://api.pirsch.io/pirsch.js',
+                            id: 'pirschjs',
+                            defer: true,
+                            'data-code': process.env.PIRSCH_CODE,
+                            type: 'text/javascript',
+                        },
+                    ]
+                    : []),
+            ],
+        },
+    },
+
+    extends: [
+        'github:hlassiege/bloggrify',
+        '@bloggrify/bento'
+    ],
+})
